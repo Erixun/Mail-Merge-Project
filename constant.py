a6e1dc70c53f2f07b50690c9d18671c74127f8b2
Erik Sundberg <erik.emanuel.sundberg@outlook.com>
2024-03-26 20:09:07 +0100

    feat: Separate constants

---
+++ constant.py
@@ -0,0 +1,4 @@
+PATH_NAMES = "Input/Names/invitees.txt"
+PATH_TEMPLATE = "Input/Letters/template.txt"
+PATH_OUTPUT = "Output/ReadyToSend"
+PLACEHOLDER = "[name]"
--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-path_names = "Input/Names/invitees.txt"
-path_template = "Input/Letters/template.txt"
-path_output = "Output/ReadyToSend"
+from constant import PATH_NAMES, PATH_OUTPUT, PATH_TEMPLATE, PLACEHOLDER
 
-with open(path_names) as names_file, open(path_template) as template_file:
+with open(PATH_NAMES) as names_file, open(PATH_TEMPLATE) as template_file:
     names, template = names_file.readlines(), template_file.read()
 
     for line in names:
         name = line.strip()
-        output = template.replace("[name]", name)
-        path_letter = f"{path_output}/{name}.txt"
+        output = template.replace(PLACEHOLDER, name)
+        path_letter = f"{PATH_OUTPUT}/{name}.txt"
 
         with open(path_letter, "w") as letter:
             letter.write(output)
